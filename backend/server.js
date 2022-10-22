e5f88252bb2fa71a83c30f5454ba85fab689978f
Yannik195 <yannisim@gmail.com>
2022-10-22 15:28:27 +0200

    Routes Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,19 @@ const app = express()
 const mongoose = require("mongoose")
 require('dotenv').config()
 
+app.use(express.json())
+
 //Connect to MongoDB
 mongoose.connect(process.env.DB_CONNECT,
   () => console.log("Connected to MongoDB"))
 
 //Import Routes
-const testRoutes = require("./routes/test")
+const surveyRoutes = require("./routes/survey")
+const resultRoutes = require("./routes/result")
 
 //Route Middleware
-app.use("/api/test", testRoutes)
+app.use("/api/survey", surveyRoutes)
+app.use("/api/result", resultRoutes)
 
 //Start Server
 const PORT = process.env.PORT || 3001
