2205950eea968db8800ef213d32cef2c6f72f0b4
Yannik195 <yannisim@gmail.com>
2022-11-11 15:12:03 +0100

    Charts von chart.js

--- frontend/src/Pages/q/evaluate.js
+++ frontend/src/Pages/q/evaluate.js
@@ -1,6 +1,59 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { faker } from '@faker-js/faker';
+
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
 
 export function Evaluate() {
   const { id } = useParams()
@@ -14,10 +67,24 @@ export function Evaluate() {
       .then(data => setQuestionnaire(data));
   }, []);
 
+  let data = {
+    labels: ['Attractiveness', 'Perspicuity', 'Efficiency', 'Dependability', 'Stimulation', 'Novelty'],
+    datasets: [{
+      label: 'Mean Value Per Item',
+      data: [2.2, -1.1, 2.1, 2.4, 2.1, 1.7],
+      backgroundColor: [
+        '#FFA049',
+      ],
+      borderColor: [
+        '#ffffff'
+      ]
+    }]
+  }
 
   return (
     <header className="App-header">
       <h1>Evaluate</h1>
+      <Bar data={data} />
 
     </header>
   );
