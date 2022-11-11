9b21e9bd93b2961aada52353dce96d477fe5937f
Yannik195 <yannisim@gmail.com>
2022-11-11 16:19:51 +0100

    Demographics

--- frontend/src/Pages/q/fill.js
+++ frontend/src/Pages/q/fill.js
@@ -6,6 +6,19 @@ export function Fill() {
   const { id } = useParams()
   let items_german = [["unerfreulich", "erfreulich"], ["unverständlich", "verständlich"], ["kreativ", "phantasielos"], ["leicht zu lernen", "schwer zu lernen"], ["wertvoll", "minderwertig"], ["langweilig", "spannend"], ["uninteressant", "interessant"], ["unberechenbar", "voraussagbar"], ["schnell", "langsam"], ["originell", "konventionell"], ["behindernd", "unterstützend"], ["gut", "schlecht"], ["kompliziert", "einfach"], ["abstoßend", "anziehend"], ["herkömmlich", "neuartig"], ["unangenehm", "angenehm"], ["sicher", "unsicher"], ["aktivierend", "einschläfernd"], ["erwartungskonform", "nicht erwartungskonform"], ["ineffizient", "effizient"], ["übersichtlich", "verwirrend"], ["unpragmatisch", "pragmatisch"], ["aufgeräumt", "überladen"], ["attraktiv", "unattraktiv"], ["sympathisch", "unsympathisch"], ["konservativ", "innovativ"]]
   const [result, setResult] = useState([4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4])
+  const [demographics, setDemographics] = useState({
+    age: 0,
+    gender: 0,
+    education: 0
+  })
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setDemographics({
+      ...demographics,
+      [evt.target.name]: value
+    });
+  }
 
   const handleFormChange = (index, event) => {
     let newArray = result.map((item, i) => {
@@ -24,8 +37,8 @@ export function Fill() {
       result,
       questionnaire: id,
       subject: {
-        age: 23,
-        gender: "female"
+        age: demographics.age,
+        gender: demographics.gender
       }
     })
       .then(res => {
@@ -40,15 +53,50 @@ export function Fill() {
       <span>14.10.2022</span>
       <span>research@mail.de</span>
       <h2>Standmixer Bosch 1200W</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-      <h2>Angaben zu Person</h2>
+      <p>Beschreibung Lorem ipsum dolor sit amet. Beschreibung consectetur adipiscing elit. </p>
+
       <h3>Was ist ein Questionnaire?</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-      <h2>Fragebogen</h2>
-      <p>Um das Produkt zu bewerten, füllen Sie bitte den nachfolgenden Fragebogen aus. Er besteht aus Gegensatzpaaren von Eigenschaften, die das Produkt haben kann. Abstufungen zwischen den Gegensätzen sind durch Kreise dargestellt. Durch Ankreuzen eines dieser Kreise können Sie Ihre Zustimmung zu einem Begriff äußern.
-        Entscheiden Sie möglichst spontan. Es ist wichtig, dass Sie nicht lange über die Begriffe nachdenken, damit Ihre unmittelbare Einschätzung zum Tragen kommt.
-        Es gibt keine „richtige“ oder „falsche“ Antwort. Ihre persönliche Meinung zählt!</p>
+
       <form onSubmit={handleSubmit}>
+        <h2>Fragebogen</h2>
+        <p>Um das Produkt zu bewerten, füllen Sie bitte den nachfolgenden Fragebogen aus. Er besteht aus Gegensatzpaaren von Eigenschaften, die das Produkt haben kann. Abstufungen zwischen den Gegensätzen sind durch Kreise dargestellt. Durch Ankreuzen eines dieser Kreise können Sie Ihre Zustimmung zu einem Begriff äußern.
+          Entscheiden Sie möglichst spontan. Es ist wichtig, dass Sie nicht lange über die Begriffe nachdenken, damit Ihre unmittelbare Einschätzung zum Tragen kommt.
+          Es gibt keine „richtige“ oder „falsche“ Antwort. Ihre persönliche Meinung zählt!</p>
+
+
+        <h2>Angaben zu Person</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+
+        <label>
+          Age
+          <input
+            type="text"
+            name="age"
+            value={demographics.age}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Gender
+          <input
+            type="text"
+            name="gender"
+            value={demographics.gender}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Education
+          <input
+            type="text"
+            name="education"
+            value={demographics.education}
+            onChange={handleChange}
+          />
+        </label>
+
+
         {items_german.map((items, i) => {
           return (
             <div key={i}>
