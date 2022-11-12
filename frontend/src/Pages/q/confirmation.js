9d6e3f5008abaaa5c73d2c926e3c7e0a3aea7e49
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-12 14:27:17 +0100

    added picture women1

--- frontend/src/Pages/q/confirmation.js
+++ frontend/src/Pages/q/confirmation.js
@@ -1,3 +1,4 @@
+import image1 from "./images/image1-women.png";
 export function Confirmation() {
     return (
         <div class="wrapper">
@@ -6,7 +7,8 @@ export function Confirmation() {
                 <p>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
             </header>
             <main class="page-main">
-                
+                <img src={image1}/>
+                <h2>Du möchtest auch UEQ Daten erheben und auswerten?</h2>
             </main>
         </div>
   );
