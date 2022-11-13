e21168a823491c505a8cd06281a3e2beda372a1b
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-13 10:08:12 +0100

    added text and button on bestaetigungsseite

--- frontend/src/Pages/q/confirmation.js
+++ frontend/src/Pages/q/confirmation.js
@@ -7,9 +7,14 @@ export function Confirmation() {
                 <p>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
             </header>
             <main class="page-main">
-                <img src={image1}/>
-                <h2>Du möchtest auch UEQ Daten erheben und auswerten?</h2>
+                <img class="image1" src={image1}/>
+                <p class="text2">Du möchtest auch UEQ Daten erheben und auswerten?</p>
+                <p class="text3">Erstelle jetzt kostenlose UEQ-Fragebögen und werte sie Online aus.</p>
+                <div class="button1">
+                <button class="btn1">UEQ erstellen</button>
+                </div>
             </main>
+            <div class="Footer"></div>
         </div>
   );
 }
