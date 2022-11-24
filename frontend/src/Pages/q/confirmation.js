287af304caeac04efa1bc68062e42dfd722cabfa
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-24 08:00:47 +0100

    Added CSS modules to export CSS

--- frontend/src/Pages/q/confirmation.js
+++ frontend/src/Pages/q/confirmation.js
@@ -1,20 +1,22 @@
 import image1 from "./images/image1-women.png";
+import ConfirmationCSS from './confirmation.module.css';
+
 export function Confirmation() {
     return (
-        <div class="wrapper">
-            <header class="page-header">
-                <h1>Vielen Dank!</h1>
-                <p>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
+        <div class={ConfirmationCSS.wrapper}>
+            <header class="confirmation-page-header">
+                <h1 class={ConfirmationCSS.h1}>Vielen Dank!</h1>
+                <p class={ConfirmationCSS.p}>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
             </header>
-            <main class="page-main">
-                <img class="image1" src={image1}/>
-                <p class="text2">Du möchtest auch UEQ Daten erheben und auswerten?</p>
-                <p class="text3">Erstelle jetzt kostenlose UEQ-Fragebögen und werte sie Online aus.</p>
-                <div class="button1">
-                <button class="btn1">UEQ erstellen</button>
+            <main class="confirmation-page-main">
+                <img class={ConfirmationCSS.image1} src={image1} alt="womenpciture"/>
+                <p class={ConfirmationCSS.text2}>Du möchtest auch UEQ Daten erheben und auswerten?</p>
+                <p class={ConfirmationCSS.text3}>Erstelle jetzt kostenlose UEQ-Fragebögen und werte sie Online aus.</p>
+                <div class={ConfirmationCSS.button1}>
+                <button class={ConfirmationCSS.btn1}>UEQ erstellen</button>
                 </div>
             </main>
-            <div class="Footer"></div>
+            <div class={ConfirmationCSS.Footer}></div>
         </div>
   );
 }
