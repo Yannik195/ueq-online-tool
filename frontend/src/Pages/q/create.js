2e64c2e5040a7e23e00730bd6d3384060693783c
Yannik195 <yannisim@gmail.com>
2022-11-26 12:52:18 +0100

    Html

--- frontend/src/Pages/q/create.js
+++ frontend/src/Pages/q/create.js
@@ -10,46 +10,98 @@ export function Create() {
   } = useForm();
 
   const onSubmit = (data) => {
-    axios.post(`http://localhost:3001/api/questionnaire`, data)
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
+    console.log(data);
+    // axios.post(`http://localhost:3001/api/questionnaire`, data)
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   })
   };
 
   return (
     <header className="App-header">
       <h1>Create a new questionnaire</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Product
+
+        <label htmlFor='product'>
+          <strong>Product</strong>
+          <br />
           <input
+            id='product'
+            placeholder="Product"
             {...register("product", {
               required: true,
               maxLength: 35,
             })}
           />
+          <p>Die Bezechnung des Produkts, für das ein Questionnaire erstellt werden soll.</p>
         </label>
 
-        <label>Password
+        <label htmlFor='password'>
+          <strong>Password</strong>
+          <br />
           <input
+            id='password'
+            placeholder="Password"
             {...register("password", {
               required: true,
             })}
           />
+          <p>Das Passwort schützt die Ergebnisse.</p>
         </label>
 
-        <label>E-Mail
+        <label htmlFor="email">
+          <strong>E-Mail</strong>
+          <br />
           <input
+            id='email'
+            placeholder="Email"
             {...register("email", {
               required: true,
             })}
           />
+          <p>An die E-Mail Adresse werden Informationen über das angelegte Questionnaire gesendet.</p>
+          <p>Die E-Mail Adresse wird den Probanden für potentielle Rückfragen angezeigt.</p>
         </label>
+
+        <fieldset>
+          <legend><strong>UEQ Version</strong></legend>
+
+          <label htmlFor="version_long">
+            <input {...register("ueq_version")} type="radio" id="version_long" value="long" />
+            Long
+          </label>
+
+          <br></br>
+
+          <label htmlFor="version_short">
+            <input {...register("ueq_version")} type="radio" id="version_short" value="short" />
+            Short
+          </label>
+
+          <p>Wählen, ob du die lange oder kurze Version des UEQ verwendet werden soll.</p>
+
+        </fieldset>
+
+        <fieldset>
+          <legend><strong>Demographic Data</strong></legend>
+          <input {...register("demographics")} type="checkbox" value="age" id="demographics_age" />
+          <label htmlFor="demographics_age">Age</label>
+          <br />
+          <input {...register("demographics")} type="checkbox" value="gender" id="demographics_gender" />
+          <label htmlFor="demographics_gender">Gender</label>
+          <br />
+          <input {...register("demographics")} type="checkbox" value="education" id="demographics_education" />
+          <label htmlFor="demographics_education">Education</label>
+
+          <p>Auswählen, welche Demographischen Daten erhoben werden sollen. Die Angaben sind für die Probanden Optional.</p>
+
+        </fieldset>
 
         <button type="submit">Submit</button>
       </form>
-
     </header>
   );
 }
