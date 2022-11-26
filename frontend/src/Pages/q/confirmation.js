4cf5f13b106e1e9dd127c108a0461a5294ba850e
PascalPalitzsch <pascal.palitzsch@web.de>
2022-11-26 23:30:46 +0100

    export css with module

--- frontend/src/Pages/q/confirmation.js
+++ frontend/src/Pages/q/confirmation.js
@@ -1,22 +1,23 @@
 import image1 from "./images/image1-women.png";
-import ConfirmationCSS from './confirmation.module.css';
+import styles from './confirmation.module.css';
+
 
 export function Confirmation() {
     return (
-        <div class={ConfirmationCSS.wrapper}>
-            <header class="confirmation-page-header">
-                <h1 class={ConfirmationCSS.h1}>Vielen Dank!</h1>
-                <p class={ConfirmationCSS.p}>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
+        <div className={styles.wrapper}>
+            <header className={styles.confirmation_header}>
+                <h1 className={styles.h1}>Vielen Dank!</h1>
+                <p className={styles.p}>Dein ausgefülltes Formular wurde erfolgreich abgeschickt.</p>
             </header>
-            <main class="confirmation-page-main">
-                <img class={ConfirmationCSS.image1} src={image1} alt="womenpciture"/>
-                <p class={ConfirmationCSS.text2}>Du möchtest auch UEQ Daten erheben und auswerten?</p>
-                <p class={ConfirmationCSS.text3}>Erstelle jetzt kostenlose UEQ-Fragebögen und werte sie Online aus.</p>
-                <div class={ConfirmationCSS.button1}>
-                <button class={ConfirmationCSS.btn1}>UEQ erstellen</button>
+            <main className={styles.confirmation_main}>
+                <img className={styles.image1} src={image1} alt="womenpciture"/>
+                <p className={styles.text2}>Du möchtest auch UEQ Daten erheben und auswerten?</p>
+                <p className={styles.text3}>Erstelle jetzt kostenlose UEQ-Fragebögen und werte sie Online aus.</p>
+                <div className={styles.button1}>
+                <button className={styles.btn1}>UEQ erstellen</button>
                 </div>
             </main>
-            <div class={ConfirmationCSS.Footer}></div>
+            <div className={styles.Footer}></div>
         </div>
   );
 }
