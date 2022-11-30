db76f548372c4bb319d81337ad62fb863d99b7c0
Yannik195 <yannisim@gmail.com>
2022-11-30 02:13:56 +0100

    CSS and Html

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -15,12 +15,15 @@ export function Home() {
       <p>*kein Account erforderlich</p>
     </section>
     <section>
-      <h1>FAQ</h1>
-      <h2>How does it work</h2>
+      <h2>What is a Usability Engineering Questionnaire</h2>
+    </section>
+    <section>
+      <h2>FAQ</h2>
+      <h3>How does it work</h3>
       <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
-      <h2>Do I need an account</h2>
+      <h3>Do I need an account</h3>
       <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
-      <h2>What does it cost</h2>
+      <h3>What does it cost</h3>
       <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
     </section>
   </div>
