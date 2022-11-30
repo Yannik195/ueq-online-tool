772bdb807626e08a28fbebaac627b63e14a6be53
Yannik195 <yannisim@gmail.com>
2022-11-30 01:52:36 +0100

    html

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,3 +1,8 @@
 export function Home() {
-  return <h1>Home</h1>
+  return <div>
+    <h1>Jetzt online UEQ Fragebogen anlegen und analysieren</h1>
+    <p>lorem ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet, consectetur adip.</p>
+    <button>Jetzt kostenlos UEQ erstellen*</button>
+    <p>*kein Account erforderlich</p>
+  </div>
 }
