c425a6b049ddc4aa5f5390047e24d1f1e2ebf7c2
Yannik195 <yannisim@gmail.com>
2022-11-30 02:08:04 +0100

    remove about page

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -1,6 +1,5 @@
 import { Routes as RoutesDom, Route } from "react-router-dom";
 import { Home } from "../Pages/Home"
-import { About } from "../Pages/About"
 import { Fill } from "../Pages/q/fill"
 import { Create } from "../Pages/q/create"
 import { Evaluate } from "../Pages/q/evaluate"
@@ -9,7 +8,6 @@ export default function Routes() {
   return (
     <RoutesDom>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
       <Route path="/q/fill/:id" element={<Fill />} />
       <Route path="/q/create/" element={<Create />} />
       <Route path="/q/evaluate/:id" element={<Evaluate />} />
