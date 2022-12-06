ef10a0f565c353944bec8103b18319f3d171c1c6
Vo Hoang Phuc <hv015@hdm-stuttgart.de>
2022-12-06 20:24:47 +0100

    dropdown menu icon/flags

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from "react-i18next";
+import HttpApi from 'i18next-http-backend';
 import {
   BrowserRouter
 } from "react-router-dom";
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'flag-icons/css/flag-icons.min.css'
 
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'de', 'fr'],
+     
+    fallbackLng: "en",
+    debug: false,
+    detection: {
+      order: [ 'cookie', 'htmlTag', 'localStorage','path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translations.json',
+    },
+   react: { useSuspense: false},
+  });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
