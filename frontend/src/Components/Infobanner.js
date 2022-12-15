294e2dd56444f40dc5ee0540b60ceecc02790a8a
LilJane96 <stefanelli1996@googlemail.com>
2022-12-15 21:03:21 +0100

    Changing the position of the icon

--- frontend/src/Components/Infobanner.js
+++ frontend/src/Components/Infobanner.js
@@ -18,24 +18,21 @@ const Infobanner = (props) => {
     window.localStorage.setItem('Hide_Infobanner', JSON.stringify(showBanner));
   }, [showBanner]);
 
-
   return (
     <div className='test'>
       {showBanner && (
-        <div className="infobanner">
-          <div className="btnimg">
+        <div className="infobanner">  
             <FontAwesomeIcon className='img' icon={faQuestion} />
-          </div>
           <div className='text'>
             <div className="title">
               {title}
             </div>
             <div className="content">{content}</div>
           </div>
-          <div className='hint'>
-            <input type="checkbox" onClick={() => setShowBanner(false)}></input>
-            {' '} Don't show hint anymore
-          </div>
+          <div className='hint'> 
+            Don't show hint anymore
+            <input className='checkmark' type="checkbox" onClick={() => setShowBanner(false)}></input>
+            </div>
         </div>
       )}
     </div>
