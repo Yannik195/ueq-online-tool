0a6233bc8c0a9e9e47e11ba242c593eb2fcc3e5d
Yannik195 <yannisim@gmail.com>
2022-12-18 17:38:10 +0100

    navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,16 +1,19 @@
 import styles from './Navbar.module.scss'
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
+      <ul className={styles.ul}>
+        <Link to="/" className={styles.logo}>
+          <FontAwesomeIcon icon={faClipboard} />
+          <li>UEQ Online</li>
         </Link>
         <Link to="/q/create">
-          <li>Create UEQ</li>
+          <button className={styles.button}>Create UEQ</button>
         </Link>
       </ul>
     </div>
