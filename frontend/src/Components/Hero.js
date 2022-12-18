d10dc751a702ab242558c1634203edd739a489d3
Yannik195 <yannisim@gmail.com>
2022-12-18 18:48:47 +0100

    hero

---
+++ frontend/src/Components/Hero.js
@@ -0,0 +1,22 @@
+import styles from "./Hero.module.scss"
+import { useNavigate } from "react-router-dom";
+
+
+export default function Hero() {
+  let navigate = useNavigate();
+
+  return (
+    <section className={styles.hero}>
+
+      <div class="top">
+        <h1 className={styles.h1}><span className={styles.orange}>Collect</span> and <span className={styles.orange}>evaluate</span> your UEQ results </h1>
+        <p className={styles.p}>This easy-to-use tool streamlines your daily tasks and saves you time, so you can focus on what matters most. Try UEQ today and see how it can make a difference for you.</p>
+      </div>
+      <div class="bottom">
+        <button className={styles.button} onClick={() => navigate("q/create")}>Jetzt kostenlos UEQ erstellen*</button>
+        <p>*kein Account erforderlich</p>
+      </div>
+
+    </section>
+  )
+}
