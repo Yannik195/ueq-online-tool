c17d15829d593e9364f302d09d4435f34b6c7ad2
Yannik Simon <31849654+Yannik195@users.noreply.github.com>
2022-12-27 14:04:16 +0100

    Add Landingpage and Team Pictures (#39)
    * Add Landingpage and Team Pictures
    
    * 12 bestaetigungsseite (#38)
    
    * package-lock.json
    
    * package-lock.json
    
    * Added confirmation site and route to confirmation
    
    * added page-header on confirmation page
    
    * added picture women1
    
    * added text and button on bestaetigungsseite
    
    * Added CSS modules to export CSS
    
    * export css with module
    
    * added react route for button UEQ erstellen on confirmation page
    
    * added mobile view with css media queries
    
    * postmark send email with link and password to find the ueq
    
    * gitignore
    
    * Added variables to email js
    
    * email js
    
    * remove node from root
    
    * css to scss
    
    * css to scss
    
    * added postmark variables
    
    * added postmark email service and using .env for the API Key and added the final link to the Email
    
    * added comments
    
    * remove nodemodules from root
    
    * 22 edit page (#30)
    
    * package-lock.json
    
    * package-lock.json
    
    * Infobanner
    
    * Edit Page
    
    * send data from popup to backend
    
    * add todos
    
    * add new function 'reloadPage'
    
    * merge
    
    * remove reload page to axios call
    
    * package json
    
    * remove app.css
    
    * remove comments from infoaberscss
    
    * remove styling from index.scss
    
    * remove images
    
    * Move infobanner to components
    
    * move infobanner to components
    
    * remove whitespace
    
    * delete comments
    
    * Changing the position of the icon
    
    * update product name
    
    * typo
    
    * bugfix
    
    * Change distances
    
    * pass prop instead of api call in popup
    
    * styles
    
    * g
    
    * find mistake
    
    * delete wronge infobanner
    
    Co-authored-by: LilJane96 <stefanelli1996@googlemail.com>
    
    * navbar
    
    * hero
    
    * favicon
    
    * moved email send
    
    * fix gitignore
    
    * inline function
    
    Co-authored-by: PascalPalitzsch <pascal.palitzsch@web.de>
    Co-authored-by: LilJane96 <stefanelli1996@googlemail.com>
    
    * fix confirmation
    
    * 29 evaluate (#40)
    
    * changes
    
    * Remove untransformed values
    
    * Distribution of Answers Chart
    
    * Mean Value Per Item Chart
    
    * Participants
    
    * Results Table
    
    * Results Table Styles
    
    * fix chart
    
    * FAQ
    
    * Team
    
    Co-authored-by: LilJane96 <stefanelli1996@googlemail.com>
    Co-authored-by: PascalPalitzsch <pascal.palitzsch@web.de>

---
+++ frontend/src/Components/Team.js
@@ -0,0 +1,107 @@
+import styles from "./Team.module.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
+export default function Hero() {
+  let yannik = require('../images/yannik.jpeg');
+  let liljana = require('../images/liljana.png')
+  let pasi = require('../images/pasi.jpg')
+  let phuc = require('../images/phuc.jpg')
+
+  const team = [
+    {
+      name: "Yannik",
+      image: yannik,
+      title: "Web Developer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      links: [
+        {
+          type: "Github",
+          icon: faGithub,
+          link: "https://github.com/Yannik195"
+        },
+        {
+          type: "LinkedIn",
+          icon: faLinkedin,
+          link: "https://www.linkedin.com/in/yannik-simon-498b50192/"
+        }
+      ]
+    },
+    {
+      name: "Pasi",
+      image: pasi,
+      title: "Web Developer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      links: [
+        {
+          type: "Github",
+          icon: faGithub,
+          link: ""
+        }
+      ]
+    },
+    {
+      name: "Liljana",
+      image: liljana,
+      title: "Web Developer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      links: [
+        {
+          type: "Github",
+          icon: faGithub,
+          link: ""
+        }
+      ]
+    },
+    {
+      name: "Phuc",
+      image: phuc,
+      title: "Web Developer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      links: [
+        {
+          type: "Github",
+          icon: faGithub,
+          link: ""
+        }
+      ]
+    },
+  ]
+
+
+  return (
+    <section className={styles.team}>
+      <h1>Team</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div className={styles.container}>
+        {team.map((person, i) => {
+          return (
+            <div className={styles.card} key={i}>
+              <div className={styles.top}>
+                <img src={person.image}></img>
+                <div>
+                  <h2>{person.name}</h2>
+                  <div className={styles.title}>
+                    <p>{person.title}</p>
+                    <div className={styles.circle} />
+                    {person.links.map((link, i) => {
+                      console.log(link);
+                      return (
+                        <a href={link.link}>
+                          <FontAwesomeIcon className={styles.icon} icon={link.icon} />
+                        </a>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+              <p>{person.description}</p>
+            </div>
+          )
+        })}
+      </div>
+
+    </section>
+  )
+}
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,20 +1,39 @@
 import Hero from "../Components/Hero"
+import styles from "../scss/Home.module.scss"
+import { useNavigate } from "react-router-dom";
+import Team from "../Components/Team"
+import FAQ from "../Components/FAQ"
 
 export function Home() {
-  return <div>
-    <Hero></Hero>
+  let navigate = useNavigate();
 
-    <section>
-      <h2>What is a Usability Engineering Questionnaire</h2>
-    </section>
-    <section>
-      <h2>FAQ</h2>
-      <h3>How does it work</h3>
-      <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
-      <h3>Do I need an account</h3>
-      <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
-      <h3>What does it cost</h3>
-      <p>lorem-ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet</p>
-    </section>
-  </div>
+  return (
+    <div>
+      <Hero></Hero>
+      <FAQ></FAQ>
+      <Team></Team>
+
+
+      {/*       <section className={styles.questionnaire}>
+        <section className={styles.backgroundText}>
+          <div class="top">
+            <h1 className={styles.h1}><span className={styles.orange}>User Experience Questionnaire</span> </h1>
+            <p className={styles.p}>
+              The <span className={styles.orange}>User Experience Questionnaire (UEQ)</span> is an established questionnaire for measuring the user experience of interactive products. The questionnaire
+              measures user experience on the dimensions of efficiency, transparency, controllability, stimulation, originality, and attractiveness.
+              It makes your product comparable with competing products.
+
+              Do you have a product that you would like to test for usability by having it tested and evaluated by users?
+              With a few simple clicks you can describe your product and save it via a mail of your choice. With the randomly
+              generated link, which you forward to the users, you can now easily have your product evaluated. As soon as you log back in with your mail, you can view the
+              evaluation of your product in graphs. The questionnaires can also be translated into different languages.</p>
+          </div>
+          <div class="bottom">
+            <button className={styles.button} onClick={() => navigate("q/create")}>Jetzt kostenlos UEQ erstellen*</button>
+            <div className={styles.info}>*kein Account erforderlich</div>
+          </div>
+        </section>
+      </section> */}
+    </div>
+  )
 }
