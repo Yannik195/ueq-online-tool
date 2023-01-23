7bbabaac4eafdbeb6a3d75ed709bebc1c045b8a3
Yannik Simon <31849654+Yannik195@users.noreply.github.com>
2023-01-23 11:19:20 +0100

    implement page view analytics (#54)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,19 @@ import styles from './App.module.scss';
 import Routes from "./Routes/Routes"
 import Navbar from "./Components/Navbar"
 import Footer from './Components/Footer';
-
-
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  //Google Analytics
+  ReactGA.initialize("G-FYSSFHKG03");
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <div className="App">
       <Navbar className={styles.navbar} />
